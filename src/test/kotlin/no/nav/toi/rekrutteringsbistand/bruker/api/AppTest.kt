eed16514c9050fef21d8eba13ceab78b97e675c4
Martine Erdal Mansaker <martine.erdal.mansaker@nav.no>
2025-08-26 13:22:05 +0200

    Fjerne test til gammel app-fil

--- src/test/kotlin/no/nav/toi/rekrutteringsbistand/bruker/api/AppTest.kt
+++ src/test/kotlin/no/nav/toi/rekrutteringsbistand/bruker/api/AppTest.kt
@@ -8,9 +8,4 @@ import kotlin.test.assertNotNull
 
 
 class AppTest {
-    @Test
-    fun appHasAGreeting() {
-        val classUnderTest = App()
-        assertNotNull(classUnderTest.greeting, "app should have a greeting")
     }
-}
