1a78def6f8659fc65f96ad6bca05fe62deb0616e
Erlend Wiklem <admin@wiklem.no>
2026-02-16 22:20:20 +0100

    Alter table

--- src/main/resources/db/migration/V4__tilbakemeldinger.sql
+++ src/main/resources/db/migration/V4__tilbakemeldinger.sql
@@ -6,5 +6,5 @@ CREATE TABLE tilbakemeldinger (
     status VARCHAR(50) NOT NULL DEFAULT 'NY',
     trelloLenke VARCHAR(1024),
     kategori VARCHAR(50) NOT NULL,
-    url VARCHAR(1024)
+    url VARCHAR(1024) NOT NULL
 );
